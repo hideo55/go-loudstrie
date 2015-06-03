5ac2db18a52fd8079741af07d2f7a657fbfde1a9
Hideaki Ohno <hide.o.j55@gmail.com>
2015-06-03 11:43:33 +0900

    Add test pattern(for tail-trie)

--- loudstrie_test.go
+++ loudstrie_test.go
@@ -13,7 +13,16 @@ func TestBuild(t *testing.T) {
 		"abcde",
 		"can",
 	}
-	trie, err := builder.Build(keyList, false)
+
+	trie, err := builder.Build(keyList, true)
+	if err != nil {
+		t.Error("Build error")
+	}
+	if trie.GetNumOfKeys() != uint64(len(keyList)) {
+		t.Error("")
+	}
+
+	trie, err = builder.Build(keyList, false)
 	if err != nil {
 		t.Error("Build error")
 	}
@@ -31,17 +40,24 @@ func TestExactMatchSearch(t *testing.T) {
 		"abcde",
 		"can",
 	}
-	trie, _ := builder.Build(keyList, true)
-	for _, key := range keyList {
-		id := trie.ExactMatchSearch(key)
-		decode := trie.DecodeKey(id)
-		if key != decode {
-			t.Error("Expected", key, "got", decode)
+
+	trie1, _ := builder.Build(keyList, true)
+	trie2, _ := builder.Build(keyList, false)
+	tries := []*Trie{&trie1, &trie2}
+
+	for _, trie := range tries {
+
+		for _, key := range keyList {
+			id := (*trie).ExactMatchSearch(key)
+			decode := (*trie).DecodeKey(id)
+			if key != decode {
+				t.Error("Expected", key, "got", decode)
+			}
 		}
-	}
-	id := trie.ExactMatchSearch("aaa")
-	if id != NotFound {
-		t.Error("Expected", NotFound, "got", id)
+		id := (*trie).ExactMatchSearch("aaa")
+		if id != NotFound {
+			t.Error("Expected", NotFound, "got", id)
+		}
 	}
 }
 
@@ -54,23 +70,30 @@ func TestCommonPrefixSearch(t *testing.T) {
 		"abcde",
 		"can",
 	}
-	trie, _ := builder.Build(keyList, true)
-	results := trie.CommonPrefixSearch("abcde", 0)
-	if len(results) != 2 {
-		t.Error(results)
-	}
-	str := trie.DecodeKey(results[0].ID)
-	if str != "abc" {
-		t.Error(str)
-	}
-	str = trie.DecodeKey(results[1].ID)
-	if str != "abcde" {
-		t.Error(str)
-	}
 
-	results = trie.CommonPrefixSearch("abcde", 1)
-	if len(results) != 1 {
-		t.Error(results)
+	trie1, _ := builder.Build(keyList, true)
+	trie2, _ := builder.Build(keyList, false)
+	tries := []*Trie{&trie1, &trie2}
+
+	for _, trie := range tries {
+
+		results := (*trie).CommonPrefixSearch("abcde", 0)
+		if len(results) != 2 {
+			t.Error(results)
+		}
+		str := (*trie).DecodeKey(results[0].ID)
+		if str != "abc" {
+			t.Error(str)
+		}
+		str = (*trie).DecodeKey(results[1].ID)
+		if str != "abcde" {
+			t.Error(str)
+		}
+
+		results = (*trie).CommonPrefixSearch("abcde", 1)
+		if len(results) != 1 {
+			t.Error(results)
+		}
 	}
 }
 
@@ -83,27 +106,31 @@ func TestPredictiveSearch(t *testing.T) {
 		"abcde",
 		"can",
 	}
-	trie, _ := builder.Build(keyList, true)
+	trie1, _ := builder.Build(keyList, true)
+	trie2, _ := builder.Build(keyList, false)
+	tries := []*Trie{&trie1, &trie2}
 
-	results := trie.PredictiveSearch("ab", 0)
-	if len(results) != 3 {
-		t.Error(results)
-	}
+	for _, trie := range tries {
+		results := (*trie).PredictiveSearch("ab", 0)
+		if len(results) != 3 {
+			t.Error(results)
+		}
 
-	results = trie.PredictiveSearch("ab", 1)
-	if len(results) != 1 {
-		t.Error(results)
-	}
-	results = trie.PredictiveSearch("can", 0)
-	if len(results) != 1 {
-		t.Error(results)
-	}
-	results = trie.PredictiveSearch("d", 0)
-	if len(results) != 0 {
-		t.Error(results)
-	}
-	results = trie.PredictiveSearch("cas", 0)
-	if len(results) != 0 {
-		t.Error(results)
+		results = (*trie).PredictiveSearch("ab", 1)
+		if len(results) != 1 {
+			t.Error(results)
+		}
+		results = (*trie).PredictiveSearch("can", 0)
+		if len(results) != 1 {
+			t.Error(results)
+		}
+		results = (*trie).PredictiveSearch("d", 0)
+		if len(results) != 0 {
+			t.Error(results)
+		}
+		results = (*trie).PredictiveSearch("cas", 0)
+		if len(results) != 0 {
+			t.Error(results)
+		}
 	}
 }
